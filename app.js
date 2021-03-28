9d82bd94d6fcb585a343273e95dd2b694cb9d810
Anna <mambarinka@mail.ru>
2021-03-28 19:20:31 +0400

    refactor: adding tag selectors according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -32,19 +32,23 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
 
+    listItem.className='task-list-item';
+
     label.innerText=taskString;
-    label.className='task';
+    label.className='task-list-label';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className="input task-list-checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="input task-list-input task-list-input_show";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="button edit";
 
-    deleteButton.className="delete";
+    deleteButton.className="button delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className="task-list-button-img";
     deleteButton.appendChild(deleteButtonImg);
 
 
